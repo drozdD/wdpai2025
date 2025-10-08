45b27501957433863aae33c350ce9c36f80bed06
drozdD <jdrozd2003@gmail.com>
2025-10-08 08:39:47 +0200

    lab 02

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo "<h1>Hello world 💪</h1>";
+// echo "<h1>Hello world 💪</h1>";
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+switch ($path) {
+    case 'dashboard':
+        include 'public/views/dashboard.html';
+        break;
+    case 'login':
+        include 'public/views/login.html';
+        break;
+    default:
+    include 'public/views/404.html';
+        break;
+}
