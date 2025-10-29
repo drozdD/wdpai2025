65117666e1d6bac39543dccb6df29ca7ca32b70e
drozdD <jdrozd2003@gmail.com>
2025-10-29 07:39:06 +0100

    lab 3 done

--- index.php
+++ index.php
@@ -1,18 +1,8 @@
 <?php
 
-// echo "<h1>Hello world 💪</h1>";
+require_once 'Routing.php';
 
 $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
 
-switch ($path) {
-    case 'dashboard':
-        include 'public/views/dashboard.html';
-        break;
-    case 'login':
-        include 'public/views/login.html';
-        break;
-    default:
-    include 'public/views/404.html';
-        break;
-}
+Routing::run($path);
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,5 @@
-const header = document.querySelector('#title');
+const header = document.querySelector('h1');
 console.log(header);
-
 header.addEventListener('click', () => {
-   header.style.color = 'green';
+    header.style.color = 'green';
 });
